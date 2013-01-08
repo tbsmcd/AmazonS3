74f49dcd389d320ee151fc5a45356b54630c681f
tbsmcd <tbsmcd@gmail.com>
2013-01-08 16:41:18 +0900

    メモ代わり

--- Model/Behavior/S3Behavior.php
+++ Model/Behavior/S3Behavior.php
@@ -1,63 +1,91 @@
 <?php
 
-App::uses('HttpSocket', 'Network/Http');
 App::uses('File', 'Utility');
-App::uses('Xml', 'Utility');
+App::import('Vendor', 'Autoload', array('file' => 'Aws' . DS . 'vendor' . DS . 'autoload.php'));
+
+use Aws\Common\Aws;
+use Aws\S3\Enum\CannedAcl;
+use Aws\S3\Exception\S3Exception;
 
 class S3Behavior extends ModelBehavior {
 	public $s3Url = 's3.amazonaws.com';
+
 	public $accessKey;
 	public $secretKey;
-
 	public $region = null;
 
-	public $contentType;
-	public $contentLength = 0;
-	public $checkSum;
-	public $body = null;
-	public $signature;
-
-	public $localFilePath;
-
-	public function getCheckSum() {
-		$file = new File($this->localFilePath);
-		$this->checkSum = base64_encode($file->md5());
-		if (!empty($this->checkSum)) {
-			return true;
-		} else {
-			//error
-			return false;
+	/*
+	* public $actsAs = array(
+	* 	'AmazonS3.S3' => array(
+	*		'key' => '**********',
+	* 		'secret' => '***********',
+	*		'region' => 'ap-northeast-1'
+	* 	),
+	* );
+	*/
+	public function setup(Model $Model, $config = array()) {
+		$this->accessKey = $config['key'];
+		$this->secretKey = $config['secret'];
+		if (isset($config['region'])) {
+			$this->region = $config['region'];
 		}
 	}
 
-	public function getContentType() {
-		$file = new File($this->localFilePath);
-		$info = $file->info();
-		if (!empty($info['mime'])) {
-			$this->contentType = $info['mime'];
-			return true;
-		} else {
-			// error
-			return false;
-		}
+	private	function factory() {
+		$aws = Aws::factory(array(
+			'key' => $this->accessKey,
+			'secret' => $this->secretKey,
+			'region' => $this->region,
+		));
+		return $aws->get('s3');
 	}
 
-	public function createBucket($name) {
-		if (!empty($this->region)) {
-			$bodyArray = array(
-				'CreateBucketConfiguration' => array(
-					'xmlns:' => 'http://s3.amazonaws.com/doc/2006-03-01/',
-					'LocationConstraint' => $this->region,
-				),
-			);
-			$xml = Xml::fromArray($test, array('encoding' => null));
-			$body = preg_replace("/<\?.+\?>\n/", '', $xml->asXML());
-			$this->contentLength = strlen($body);
-		}
-		$request = array(
-			'method' => 'PUT',
-
+	/*
+		$file = array(
+			'name' => 'hoge',
+			'path' => 'path/to/local/file',
 		);
+	*/
+	public function addFile(Model $Model, $bucket, $file = array(), $acl = null) {
+		$s3 = $this->factory();
+		$localObject = new File($file['path']);
+		if (!isset($acl)) {
+			$acl = CannedAcl::PUBLIC_READ;
+		}
+		try {
+			$s3->putObject(array(
+				'Bucket' => $bucket,
+				'Key' => $file['name'],
+				'Body' => $localObject->read(),
+				'ACL' => $acl,
+			));
+		} catch(S3Exception $e) {
+			//error
+		}
+	}
+
+	/*
+		$files = array(
+			'hoge.jpg',
+			'huga.jpg',
+		);
+	*/
+	public function deleteFiles(Model $Model, $bucket, $files) {
+		$s3 = $this->factory();
+		$objects = array();
+		foreach ($files as $file) {
+			$objects[] = array(
+				'Key' => $file,
+			);
+		}
+		try {
+			$s3->deleteObjects(array(
+				'Bucket' => $bucket,
+				'Objects' => $objects,
+			));
+		} catch(S3Exception $e) {
+			//error
+		}
 	}
 
 }
